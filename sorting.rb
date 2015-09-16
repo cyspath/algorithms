48e9838c32ca471a215fe17c5ebaaa32fbf7bf43
Mike <lvlichaelly@gmail.com>
2015-09-15 22:24:32 -0700

    quick sort rb implemented

--- sorting.rb
+++ sorting.rb
@@ -20,3 +20,24 @@ def merge(left, right)
 end
 
 # p merge_sort([2, 4, 8, 6, 3, 10, 9, 1, 7, 5])
+
+# Quick Sort
+
+def quick_sort(arr)
+  return arr if arr.length <= 1
+  pivot = arr[0]
+  left = []
+  right = []
+  idx = 1
+  while idx < arr.length
+    if arr[idx] < pivot
+      left.push(arr[idx])
+    else
+      right.push(arr[idx])
+    end
+    idx += 1
+  end
+  quick_sort(left).concat([pivot]).concat(quick_sort(right))
+end
+
+# p quick_sort([2, 4, 8, 6, 3, 10, 9, 1, 7, 5])
