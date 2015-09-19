68d6813027848a484aab6b2f946444c6153a60eb
Mike <lvlichaelly@gmail.com>
2015-09-18 17:31:30 -0700

    contructed binary tree, added method for finding greatest depth

--- BST.rb
+++ BST.rb
@@ -1,3 +1,34 @@
+# Actual Tree for Testing
+
+class Node
+    attr_accessor :val, :left, :right
+    def initialize(val)
+        @val = val
+        @left, @right = nil, nil
+    end
+end
+
+root = Node.new(5)
+root.left = Node.new(2)
+root.left.left = Node.new(1)
+root.left.right = Node.new(4)
+root.left.right.left = Node.new(3)
+
+root.right = Node.new(7)
+root.right.left = Node.new(6)
+root.right.right = Node.new(9)
+root.right.right.left = Node.new(8)
+root.right.right.right = Node.new(10)
+
+#           5
+#       /       \
+#     2           7
+#   /   \       /   \
+# 1       4    6     9
+#       /          /   \
+#     3           8     10
+
+
 # given a node in BST, find next largest value
 
 def next_largest(node)
@@ -24,5 +55,17 @@ def next_largest(node)
       parent = parent.parent
     end
   end
+
+end
+
+# find max depth of binary tree
+def max_depth(root)
+  return 0 if root.nil?
+
+  if root.left || root.right
+    depth = 1
+  end
+
+  return depth + (max_depth(root.left), max_depth(root.right)).max
 
 end
