00d5f8f508f2eb5b7c033fa20095f71ca27f4e56
Mike <lvlichaelly@gmail.com>
2015-10-03 08:51:48 -0700

    meetup Oct-3, 2015, algorithms

--- meetup.rb
+++ meetup.rb
@@ -16,8 +16,20 @@ def flatten(arr)
 
     i += 1
   end
-  
+
   arr
 end
 
-p flatten([[1,2,[3,4]],[5,6],7,[8,[9]]])
+# p flatten([[1,2,[3,4]],[5,6],7,[8,[9]]])
+
+# binary_string
+def binary_string(n)
+  arr = []
+  while n != 0
+    arr.unshift(n % 2)
+    n /= 2
+  end
+  arr
+end
+
+binary_string(8)
