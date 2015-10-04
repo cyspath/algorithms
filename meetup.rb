8211b9696c5868be795b7bedbb4d50ba759e16cb
Mike <lvlichaelly@gmail.com>
2015-10-04 00:10:23 -0700

    added more meetup algorithm questions

--- meetup.rb
+++ meetup.rb
@@ -33,7 +33,7 @@ def binary_string(n)
   num_zero.times { arr.unshift(0) }
   arr.join()
 end
-
+# O(logn)
 # p binary_string(8)
 
 # converts from string representation of number (4) into binary
@@ -54,5 +54,42 @@ def binary_to_int(str)
   sum
 
 end
-
+# O(n) where n is lenght of string
 # p binary_to_int("0000000000111")
+
+# search for minimum in a stack in O(1) time
+class Stack
+  def initialize
+    @arr = []
+    @min_arr = []
+  end
+
+  def push(n)
+    @arr.push(n)
+    if @min_arr.last.nil? ||  n < @min_arr.last
+      @min_arr.push(n)
+    else
+      @min_arr.push(@min_arr.last)
+    end
+
+    @arr
+  end
+
+  def pop
+    @arr.pop
+    @min_arr.pop
+    @arr
+  end
+
+  def min
+    @min_arr[-1]
+  end
+end
+
+# stack = Stack.new
+# stack.push(1)
+# stack.push(3)
+# stack.push(2)
+# stack.push(-2)
+#
+# p stack.min
