c32d0a998c7b5a0bb19d2e5ce24596a932091478
Mike <lvlichaelly@gmail.com>
2015-10-22 23:10:50 -0700

    added cats with hats

---
+++ appacademy.rb
@@ -0,0 +1,17 @@
+# Cats with Hats
+# You have 100 cats. You have arranged all your cats in a line. Initially, none of your cats have any hats. You take 100 rounds walking around the cats, always starting with the first cat. Every time you stop at a cat, you put a hat on it if it doesn't have one, and you take its hat off if it has one on.
+# The first round, you stop at every cat. The second round, you only stop at every 2nd cat (#2, #4, #6, #8, etc.). The third round, you only stop at every 3rd cat (#3, #6, #9, #12, etc.). You continue this process until the 100th round (i.e. you only visit the 100th cat).
+# Write a program that prints which cats have hats at the end.
+
+def cats_with_hats
+  cats = {}
+  (1..100).each { |n| cats[n] = false }
+  (1..100).each do |step|
+    cats.keys.each do |key|
+      cats[key] = !cats[key] if key % step == 0
+    end
+  end
+  cats.select { |k,v| v == true }.keys
+end
+
+p cats_with_hats
