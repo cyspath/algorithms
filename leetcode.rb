26ff11b17a23056c014477cf494c603b3aca4f4e
Mike <lvlichaelly@gmail.com>
2015-12-12 17:36:25 -0800

    added single number III - maybe need optimization

--- leetcode.rb
+++ leetcode.rb
@@ -1,3 +1,22 @@
+# Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+#
+# For example:
+#
+# Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+#
+# Note:
+# The order of the result is not important. So in the above example, [5, 3] is also correct.
+# Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+
+def single_number(nums)
+  table = {}
+  nums.each do |n|
+    table[n] += 1 if table[n]
+    table[n] = 0 if !table[n]
+  end
+  table.select { |k,v| v == 0 }.keys
+end
+
 # Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 #
 # Solve itx without division and in O(n).
