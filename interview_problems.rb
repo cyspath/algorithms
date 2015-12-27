73898a1988d120968b6d0ed2f85241c9a640ae65
Mike <lvlichaelly@gmail.com>
2015-12-26 21:27:01 -0800

    added questions two sums O(n)

--- interview_problems.rb
+++ interview_problems.rb
@@ -1,6 +1,18 @@
 require 'byebug'
 require 'set'
 
+# The two sum problem is a common interview question, and it is a variation of the subset sum problem. There is a popular dynamic programming solution for the subset sum problem, but for the two sum problem we can actually write an algorithm that runs in O(n) time.
+#
+# The challenge is to find all the pairs of two integers in an unsorted array that sum up to a given S. For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7, your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
+
+def two_sum(arr, sum)
+  
+end
+
+p two_sum([3, 5, 2, -4, 8, 11])
+
+
+
 # The is a clever interview question that asks you to calculate the cube root of a number quickly. We can solve this by some mathematical tricks that won't require any calculates to take place, only table lookups. This algorithm will focus on calculating the cube root of 6 digit numbers (or less). For example, if the input is 636056 then your program should output 86.
 
 def cube_root(n)
