e24cc578247e926196a1f3ebccc87110ab200b30
Mike <lvlichaelly@gmail.com>
2016-06-05 20:09:19 -0700

    added meetup distance of 1

--- algos/edit_distance_is_one.rb
+++ algos/edit_distance_is_one.rb
@@ -1,16 +1,3 @@
-def edit_distance_is_one(s1,s2)
-  count = 0
-  longest = longest_str(s1, s2) # 0 is eql, -1 is s1, 1 is s2
-
-  if longest == 0
-    change_check(s1, s2)
-  elsif longest == -1
-    add_check(s2, s1)
-  elsif longest == 1
-    add_check(s1, s2)
-  end
-end
-
 def longest_str(s1, s2)
   if s1.length > s2.length
     return -1
@@ -21,24 +8,24 @@ def longest_str(s1, s2)
   end
 end
 
-def change_check(s1, s2)
+def edit_distance_is_one(s1,s2)
   count = 0
-  i = 0
-  while i < s1.length
-    count += 1 if s1[i] != s2[i]
-    i += 1
-  end
-  count == 1 ? true : count
-end
+  longest = longest_str(s1, s2) # 0 is eql, -1 is s1, 1 is s2
 
-def add_check(short, long)
   count = 0
   idx1 = 0
   idx2 = 0
-  while idx2 < long.length
-    if short[idx1] != long[idx2]
+  while idx1 < s1.length || idx2 < s2.length
+    if s1[idx1] != s2[idx2]
       count += 1
-      idx2 += 1
+      if (longest == -1)
+        idx1 += 1
+      elsif longest == 0
+        idx1 += 1
+        idx2 += 1
+      else
+        idx2 += 1
+      end
     end
     idx1 += 1
     idx2 += 1
@@ -51,3 +38,9 @@ p edit_distance_is_one("geeks", "geeks")
 p edit_distance_is_one("geeks", "geaks")
 p edit_distance_is_one("peaks", "geeks")
 p edit_distance_is_one("gks", "geeks")
+
+# true
+# 0
+# true
+# 2
+# 2
