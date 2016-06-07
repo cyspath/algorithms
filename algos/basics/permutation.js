193b3f2eaa5df4f3211faf8a6bcc12e8ba1e2182
Mike <lvlichaelly@gmail.com>
2016-06-06 21:25:15 -0700

    added permu js

---
+++ algos/basics/permutation.js
@@ -0,0 +1,19 @@
+function permutation(str) {
+  var arr = str.split("")
+  var list = []
+  recurse(arr, "", list)
+  return list
+}
+
+function recurse(arr, accum, list) {
+  if (arr.length == 0) { list.push(accum) }
+
+  arr.forEach(function (el, idx) {
+    var newArr = arr.slice()
+    var newAccum = accum + newArr.splice(idx, 1)
+    recurse(newArr, newAccum, list)
+  })
+}
+
+
+console.log(permutation('abcd'))
