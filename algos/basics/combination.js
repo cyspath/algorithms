fcda5d65ec6504d0aa93945f55e568ba9fcd3790
Mike <lvlichaelly@gmail.com>
2016-06-06 22:04:07 -0700

    added combin js

--- algos/basics/combination.js
+++ algos/basics/combination.js
@@ -1,8 +1,8 @@
 function combination(str) {
   if (str.length == 0) { return [""] }
-  
-  var newStr = str.slice(1,str.length)
-  var current = str.slice(0,1)
+
+  var newStr = str.slice(1,str.length)  // rest of str
+  var current = str.slice(0,1)          // first letter of str
 
   var result = combination(newStr)
   var newResult = result.map(function (el) {
