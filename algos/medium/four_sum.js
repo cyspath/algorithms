1422ca5d122cbb761659b4d4d0cbcc0e7e5435a8
Mike <lvlichaelly@gmail.com>
2016-06-10 21:45:49 -0700

    added prob soln for 4sum

---
+++ algos/medium/four_sum.js
@@ -0,0 +1,34 @@
+function abcd() {
+  var hash = {}
+  for (var i = 0; i < 50; i++) {
+    for (var j = i + 1; j < 50; j++) {
+      var sum = Math.pow(i, 3) + Math.pow(j, 3)
+      if (hash[sum]) {
+        hash[sum].push([i, j])
+      } else {
+        hash[sum] = [[i, j]]
+      }
+    }
+  }
+
+  var result = []
+  for (var key in hash) {
+    if (hash.hasOwnProperty(key) && hash[key].length > 1) {
+      // console.log([key, hash[key]]);
+      result = result.concat(combination(hash[key]))
+    }
+  }
+  return result
+}
+
+function combination(arr) {
+  var result = []
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      result.push(arr[i].concat(arr[j]))
+    }
+  }
+  return result
+}
+
+console.log(abcd());
