53eabefcc8a8b52bfe232c1439295171fc1e73a1
Mike <lvlichaelly@gmail.com>
2016-06-24 22:39:31 -0700

    permu

--- algos/basics/permutation.js
+++ algos/basics/permutation.js
@@ -18,14 +18,30 @@
 
 //  O(n!)
 
+// function perm(str, accum) {
+//   if (str.length === 0 ) {
+//     return [accum]
+//   }
+//   var result = []
+//   str.split("").forEach(function (el, idx) {
+//     result = result.concat(perm(str.slice(0, idx) + str.slice(idx + 1, str.length), accum + el))
+//   })
+//   return result
+// }
+// console.log(perm('abcd', ""))
+
+
 function perm(str, accum) {
-  if (str.length === 0 ) {
-    return [accum]
+  if (str.length == 0) {
+    return [accum];
   }
-  var result = []
-  str.split("").forEach(function (el, idx) {
-    result = result.concat(perm(str.slice(0, idx) + str.slice(idx + 1, str.length), accum + el))
-  })
-  return result
+  var result = [];
+  for (var i = 0; i < str.length; i++) {
+    var newAccum = accum + str[i];
+    var newStr = str.slice(0,i) + str.slice(i + 1);
+    result = result.concat(perm(newStr, newAccum));
+  }
+  return result;
 }
-console.log(perm('abcd', ""))
+
+console.log(perm('abc', ''));
