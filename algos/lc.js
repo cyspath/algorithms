0f2c0be7ad366968583b8886dffc4d153904718a
Michael Li <lvlichaelly@gmail.com>
2017-08-15 20:26:12 -0700

    update

--- algos/lc.js
+++ algos/lc.js
@@ -38,3 +38,40 @@ var distributeCandies = function(candies) {
 		return sis_candies - ((sis_candies - bro_candies) / 2)
 	}
 };
+
+
+// You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. 
+// Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, 
+// and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water 
+// inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid 
+// is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+// Example:
+
+// [[0,1,0,0],
+//  [1,1,1,0],
+//  [0,1,0,0],
+//  [1,1,0,0]]
+
+// Answer: 16
+
+var islandPerimeter = function(grid) {
+	var el, p = 0, prev_count = 0, current_count, current_cells, connected;
+	// each level will calc its max perimeter, then compare with previous level and subtract the level with min num blocks
+	for (var i = 0; i < grid.length; i++) {
+		current_count = 0;
+		current_cells = [];
+		connected = false;
+		for (var j = 0; j < grid[i].length; j++) {
+			el = grid[i][j];
+			if (el === 1 && connected) {
+				current_cells[current_cells.length - 1]
+			}
+
+		}
+		// now subtract the joints
+
+	}
+};
+
+
